1e340126e7f82c34169fd631508c263c2c22f59a
Carlo Bruno <carlofbruno@gmail.com>
2024-05-13 10:46:03 -0500

    create server.js, will convert to ts later

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import next from "next";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3010;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    // ...
+    console.log("🔥 connection");
+  });
+
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
